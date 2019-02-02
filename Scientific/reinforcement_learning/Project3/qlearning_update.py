d5d10744fb0417b12094385923b65d71e55e566a
MiladInk <mi.aghajohari@gmail.com>
2019-02-02 18:02:24 +0330

    add alpha

--- Scientific/reinforcement_learning/Project3/qlearning_update.py
+++ Scientific/reinforcement_learning/Project3/qlearning_update.py
@@ -1,3 +1,4 @@
 def qlearning_update(self, state, action, nextState, reward):
-    estimatedQ = reward + self.computeValueFromQValues(nextState)
+    self.alpha = 0.9
+    estimatedQ = self.alhpa * (reward + self.computeValueFromQValues(nextState)) + (1-self.alpha)*self.qvalues[(state,action)]
     self.qValues[(state,action)] = estimatedQ
